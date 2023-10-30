3399fa591a518b8a60fb50fa70cca13f38d8d0bd
Christopher Tineo <christophertineo02@gmail.com>
2023-10-30 10:32:25 -0400

    project init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Deploy lambda function with Github Actions
+
+This project is intended to build a Github Actions deploy pipeline for a simple AWS Lambda function in python.
---
+++ init.py
@@ -0,0 +1,16 @@
+def lambda_handler(event, context):
+    """Lambda function wrapper
+    Args:
+        event: trigger event dict
+        context: lambda methods and properties
+    Returns:
+        string: greeting response
+    """
+    print('Starting functions\n---------------------------------------------')
+
+    if event["input"] == "Hello":
+
+        return "World"
+
+    else:
+        raise
